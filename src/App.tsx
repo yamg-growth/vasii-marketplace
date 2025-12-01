033c7d3d0ea7327f5a791b28861450892d865a80
yamg-growth <yuliet.mg11@gmail.com>
2025-12-01 12:44:13 -0500

    Remove AdminUpload route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import { PromotionBanner } from "./components/PromotionBanner";
-import AdminUpload from "./pages/AdminUpload";
 
 const queryClient = new QueryClient();
 
@@ -27,7 +26,7 @@ const App = () => (
         <Routes>
           {/* Login route (no layout) */}
           <Route path="/login" element={<Login />} />
-          <Route path="/admin-upload" element={<AdminUpload />} />
+         
           {/* Protected admin route (no public layout) */}
           <Route path="/admin" element={
             <ProtectedRoute>
